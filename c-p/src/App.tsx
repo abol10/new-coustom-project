19e2ca1ac07f13cf323af86b31eebe64f4040256
neon <nono@gmail.com>
2024-11-10 15:03:06 +0330

    project App

---
+++ c-p/src/App.tsx
@@ -0,0 +1,15 @@
+import Heders from './components/pages1/heders'
+import Section from './components/pages2/section'
+import './index.css'
+
+function App() {
+ 
+  return (
+     <div className=''>
+      <Heders />
+      <Section />
+     </div>
+  )
+}
+
+export default App
